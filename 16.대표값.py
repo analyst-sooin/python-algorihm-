c7f59d3339ed385f4c3eb0df6ecefce6f62574a9
Sooin Kim <59334939+analyst-sooin@users.noreply.github.com>
2021-07-01 00:21:06 +0900

    Update 16.대표값.py

--- 16.대표값.py
+++ 16.대표값.py
@@ -1,19 +1,17 @@
 import sys
-sys.stdin=open("input.txt", "r")
-n=int(input())
-a=list(map(int, input().split()))
-ave=sum(a)/n
-ave=ave+0.5
-ave=int(ave)
-min=2147000000
-for idx, x in enumerate(a):
-    tmp=abs(x-ave)
-    if tmp<min:
-        min=tmp
-        score=x
-        res=idx+1
-    elif tmp==min:
-        if x>score:
-            score=x
-            res=idx+1
-print(ave, res)
+sys.stdin = open('input.txt', 'rt')
+
+n = int(input())
+a = list(map(int, input().split()))
+avg = round(sum(a) / n)
+min = 2147000000
+for idx , s in enumerate(a):
+    dist = abs(s - avg)
+    if dist < min:
+        min = dist
+        score = s
+        res = idx +1
+    elif dist == min:
+        if s > score:
+            score = s
+            res = idx +1
