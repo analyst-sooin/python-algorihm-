859a58f9b0eb3c549af3bcac7dc5c0de4a46a03d
kgu-bigdata <59334939+analyst-sooin@users.noreply.github.com>
2021-07-07 17:52:55 +0900

    17 update

--- 16,17.18 review.py
+++ 16,17.18 review.py
@@ -1,5 +1,5 @@
 
-
+'''
 import sys
 sys.stdin = open('input.txt','rt')
 
@@ -20,3 +20,16 @@ for idx,s in enumerate(a):
             score = s
             res = idx+1
 print(avg,res)
+'''
+
+import sys 
+sys.stdin = open('input.txt','rt')
+
+n,m = map(int, input().split())
+a = [0] *(n+m+3)
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        a[i+j] +=1
+
+print(a)
