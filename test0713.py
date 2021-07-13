29ee095ec8d6b18662d7d175c8621cabbdf4b5a5
kgu-bigdata <59334939+analyst-sooin@users.noreply.github.com>
2021-07-13 13:43:11 +0900

    test1

---
+++ test0713.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt', 'rt')
+def digit_sum(x):
+    sum = 0
+    while x > 0:
+        sum =+ x % 10
+        x = x//10
+    return sum
+
+n = int(input())
+a = list(map(int, input().split()))
+max = -2147000000
+res = 0
+for x in a:
+    tot = digit_sum(x)
+    if tot > max:
+        max = tot
+        res = x
+print(x)
