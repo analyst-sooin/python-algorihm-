2ffd414e2fd78b65585448317cbd8fd2f1d004b0
kgu-bigdata <59334939+analyst-sooin@users.noreply.github.com>
2021-07-13 16:58:21 +0900

    udate

--- test0713.py
+++ test0713.py
@@ -1,16 +1,33 @@
 import sys 
-
 sys.stdin = open('input.txt', 'rt')
 
+# 숫자를 뒤집는 함수 reverse 하나
+# 소수인지 아닌지를 확인하는 함수 isPrime 하나 
+
+
 n = int(input())
-ch = [0]*(n+1)
-cnt = 0
+a = map(int,input().split())
 
-for i in range(2,n+1):
-    if ch[i] == 0:
-        cnt += 1
-    for j in range(i, n+1, i):
-        ch[j] +=1
+def reverse(x):
+    res =0
+    while x > 0:
+        t = x % 10 
+        res = res*10 +t
+        x = x//10
+    return res
+    
 
-print(ch)
-print(cnt)
+def isPrime(x):
+    if x ==1:
+        return False
+    for i in range(2, x//2+1):
+        x % i == 0
+        return False
+    else :
+        return True
+        
+
+for x in a:
+    tmp = reverse(x)
+    if isPrime(tmp):
+        print(tmp, end =' ')
